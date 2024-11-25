79c586b1d48a5300722c48e14a1cc2ecd760c185
SirEvgen <sandrofa6666666@gmail.com>
2024-11-25 06:35:41 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -21,7 +21,7 @@ async def post_user(username: Annotated[str, Path(min_length=5, max_length=20,
     return f"User {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: Annotated[int, Path(ge=1, le=100, discriminator="Enter your id",
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter your id",
                         example="12")], username: Annotated[str, Path(min_length=5, max_length=20, 
                     description='Enter username', example='UrbanUser')], 
                       age: Annotated[int, Path(ge=18, le=120, description='Enter age', 
@@ -31,7 +31,7 @@ async def update_user(user_id: Annotated[int, Path(ge=1, le=100, discriminator="
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: Annotated[str, Path(min_length=1, max_length=3, 
-                                                   discriminator='Enter your id',
+                                                   description='Enter your id',
                                                    example="12")]) -> str:
     users.pop(user_id)
     return f"User № {user_id} has been deleted"
